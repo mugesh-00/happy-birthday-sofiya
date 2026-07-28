681a2598d4662270e02addf3cc3b29d0c0f8ab1c
mugesh-00 <mugeshbalusamy23@gmail.com>
2026-07-28 13:44:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,16 @@ function showMessage() {
         msg.style.display = "block";
     }
 }
+setInterval(() => {
+    const heart = document.createElement("div");
+    heart.className = "heart";
+    heart.innerHTML = "❤️";
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.fontSize = (20 + Math.random() * 20) + "px";
+
+    document.getElementById("hearts").appendChild(heart);
+
+    setTimeout(() => {
+        heart.remove();
+    }, 5000);
+}, 300);
